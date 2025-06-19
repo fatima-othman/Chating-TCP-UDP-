1d0467b215ba231e17662d9cbe124a5a10644bdc
jihad-j <jihadjamoos489@gmail.com>
2025-06-19 19:57:39 +0300

    Add files via upload

---
+++ src/Network_Project/part1HW.java
@@ -0,0 +1,15 @@
+
+package network_project;
+
+import java.io.File;
+
+
+public class part1HW {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new p1().setVisible(true);
+        new p1().setVisible(true);
+    }
+    
+}
